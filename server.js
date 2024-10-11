9bfde8d0dfe4a403c9e01806ae3b4b2b0c767cb4
SJBM1876 <137792828+SJBM1876@users.noreply.github.com>
2024-10-11 14:46:29 +0100

    commit server.js and app.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const app = require("./src/app")
 const db = require("./db/connection");
+const Restaurant = require("./models");
 const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
-
+app.get('/restaurants', async(req, res) => {
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+})
 
 app.listen(port, () => {
     db.sync();
